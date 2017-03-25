22ce86ec8fdf1faf8a0e2775e76060951cc5720a
Riley Flynn <riley@rileyflynn.me>
2017-03-25 19:53:37 -0230

    Updated NintbotForDiscord

--- NintbotForDiscord/Plugin.py
+++ NintbotForDiscord/Plugin.py
@@ -1,11 +1,56 @@
-import os
+import logging
+
+from NintbotForDiscord.Enums import EventTypes
+from NintbotForDiscord.Permissions import Permission
+from jigsaw import JigsawPlugin
 
 __author__ = 'Riley Flynn (nint8835)'
 
 
-class BasePlugin:
+class BasePlugin(JigsawPlugin):
 
-    def __init__(self, bot_instance: "Bot", plugin_data: dict, folder: os.path):
-        self.bot = bot_instance
-        self.plugin_data = plugin_data
-        self.folder = folder
+    def __init__(self, manifest, bot_instance):
+        super(BasePlugin, self).__init__(manifest)
+
+        self.bot = bot_instance  # type: Bot.Bot
+
+        self.plugin_info = {
+            "plugin_name": self.manifest.get("name", "Unnamed Plugin"),
+            "plugin_developer": self.manifest.get("developer", "Unspecified Developer"),
+            "plugin_version": self.manifest.get("version", "0.0.0"),
+            "module_name": self.manifest.get("module_name", self.manifest.get("name", "Unnamed Plugin")),
+            "main_file": self.manifest.get("main_path", "__init__.py")
+        }
+
+        self._registered_commands = []
+        self._registered_handlers = []
+
+        self.logger = logging.getLogger(self.plugin_info["plugin_name"])
+
+    def register_command(self, name: str, description: str, method: classmethod, permission: Permission = Permission()) -> None:
+        """
+        Adds a command to the internal command registry to be auto-registered/unregistered on enable/disable
+        :param name: The command name
+        :param description: The command description
+        :param method: The method that will handle the command
+        :param permission: The permission required to use the command
+        """
+        self._registered_commands.append({
+            "command": name,
+            "description": description,
+            "required_perm": permission,
+            "plugin_info": self.plugin_info,
+            "command_handler": method
+        })
+
+    def register_handler(self, event_type: EventTypes, event_handler: classmethod) -> None:
+        """
+        Adds a handler to the internal handler registry to be auto-registered/unregistered on enable/disable
+        :param event_type: The type of event this handler will handle
+        :param event_handler: The method that will handle the event
+        """
+        self._registered_handlers.append({
+            "event_type": event_type,
+            "event_handler": event_handler,
+            "plugin": self
+        })
--- NintbotForDiscord/PluginManager.py
+++ NintbotForDiscord/PluginManager.py
@@ -1,74 +1,15 @@
-import os
-import json
-import importlib.util
-import traceback
+import jigsaw
+from NintbotForDiscord import Bot
 
-from NintbotForDiscord.Plugin import BasePlugin
-from .Exceptions import PluginNotFoundException, MultiplePluginsFoundException
 __author__ = 'Riley Flynn (nint8835)'
 
 
 class PluginManager:
 
     def __init__(self, bot):
-        self.bot = bot
-        self.plugins = []
-
-    def get_plugins_by_name(self, name: str) -> list:
-        """
-        Returns all plugins matching the name
-        :param name: The name to match
-        :return: The list of all plugins matching the name
-        """
-        return [plugin for plugin in self.plugins if plugin["info"]["plugin_name"] == name]
-
-    def get_plugin_by_name(self, name: str) -> dict:
-        """
-        Returns only one plugin matching the name
-        :param name: The name to match
-        :return: The plugin that matches the name
-        """
-        plugins = self.get_plugins_by_name(name)
-        if len(plugins) == 1:
-            return plugins[0]
-        elif len(plugins) == 0:
-            raise PluginNotFoundException("Plugin {} could not be found.".format(name))
-        else:
-            raise MultiplePluginsFoundException("{} plugins were found with the name '{}'.".format(len(plugins), name))
-
-    def get_plugin_instance_by_name(self, name: str) -> BasePlugin:
-        """
-        Returns the plugin instance that matches the name
-        :param name: The name to match
-        :return: The plugin instance that matches the name
-        """
-        return self.get_plugin_by_name(name)["instance"]
+        self.bot = bot  # type: Bot.Bot
+        self._jigsaw = jigsaw.PluginLoader()
 
     def load_plugins(self):
-        """
-        Loads all plugins from the plugins directory
-        """
-        self.plugins = []
-        for folder in [os.path.join("plugins", i) for i in os.listdir(os.path.join("plugins")) if os.path.isdir(os.path.join("plugins", i))]:
-            if os.path.isfile(os.path.join(folder, "plugin.json")):
-                with open(os.path.join(folder, "plugin.json")) as f:
-                    plugin_data = json.load(f)
-                    # noinspection PyBroadException
-                    try:
-                        spec = importlib.util.spec_from_file_location(plugin_data["module_name"], os.path.join(folder, plugin_data["main_file"]))
-                        self.bot.logger.debug("Loading plugin {} version {}, by {}...".format(plugin_data["plugin_name"], plugin_data["plugin_version"], plugin_data["plugin_developer"]))
-                        plugin = importlib.util.module_from_spec(spec)
-                        spec.loader.exec_module(plugin)
-                        plugin_instance = plugin.Plugin(self.bot, plugin_data, folder)
-                        self.plugins.append({"info": plugin_data,
-                                             "status": "loaded",
-                                             "module": plugin,
-                                             "instance": plugin_instance})
-                        self.bot.logger.debug("Plugin loaded.")
-                    except:
-                        self.bot.logger.error("Plugin {} version {}, by {} failed to load".format(plugin_data["plugin_name"], plugin_data["plugin_version"], plugin_data["plugin_developer"]))
-                        self.plugins.append({"info": plugin_data,
-                                             "status": "error",
-                                             "exception": traceback.format_exc(5),
-                                             "module": plugin})
-                        print(self.plugins[-1]["exception"])
+        self._jigsaw.load_manifests()
+        self._jigsaw.load_plugins(self.bot)
