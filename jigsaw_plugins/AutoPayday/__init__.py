9528700caef49b724f9689da39b583e5785a1889
Riley Flynn <riley@rileyflynn.me>
2017-08-11 20:56:58 -0230

    Added a plugin to automatically claim paydays in a server I am in

---
+++ jigsaw_plugins/AutoPayday/__init__.py
@@ -0,0 +1,17 @@
+import asyncio
+
+from discord import Object
+from NintbotForDiscord.Plugin import BasePlugin
+
+
+class AutoPayday(BasePlugin):
+
+    def __init__(self, manifest, bot):
+        super().__init__(manifest, bot)
+
+        self.bot.EventManager.loop.create_task(self.payday_task())
+
+    async def payday_task(self):
+        while not self.bot.is_closed:
+            await asyncio.sleep(200)
+            await self.bot.send_message(Object("333333334430187520"), "+payday")
